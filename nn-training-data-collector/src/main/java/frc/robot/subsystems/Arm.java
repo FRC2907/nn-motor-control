7c8decc3202873c247a06a9363dfbfd2903261f8
Kate Reed <josephreed2600@gmail.com>
2024-01-21 12:17:04 -0800

    computer unhappy?

--- nn-training-data-collector/src/main/java/frc/robot/subsystems/Arm.java
+++ nn-training-data-collector/src/main/java/frc/robot/subsystems/Arm.java
@@ -15,8 +15,10 @@ public class Arm  implements Subsystem {
     private Arm(int ...ids) {
         this.motor_monitors = new MotorMonitor[ids.length];
         for (int i = 0; i < ids.length; i++) {
-            CANSparkMax mc = new CANSparkMax(ids[i], MotorType.kBrushless);
-            this.motor_monitors[i] = new MotorMonitor("arm_"+i, mc.getEncoder()::getPosition, mc::get);
+            // The Machine suggested this try-thing because it doesn't like taking out the garbage I guess
+            try (CANSparkMax mc = new CANSparkMax(ids[i], MotorType.kBrushless)) {
+                this.motor_monitors[i] = new MotorMonitor("arm_"+i, mc.getEncoder()::getPosition, mc::get);
+            }
         }
     }
     private static Arm instance;
