9b5592af82178b3fe07aab142759c6bff05d1d9d
Kate Reed <josephreed2600@gmail.com>
2024-01-21 17:40:35 -0800

    write data to file

--- entrypoint.py
+++ entrypoint.py
@@ -1,14 +1,17 @@
 print("hello world")
 
 import grpc
+from google.protobuf import text_format
 import training_data_pb2
 import training_data_pb2_grpc
 
 def run():
-	with grpc.insecure_channel('localhost:5800') as channel:
+	with grpc.insecure_channel('localhost:5800') as channel, open('/tmp/nn-data', 'w') as file:
 		stub = training_data_pb2_grpc.TrainingDataStub(channel)
-		for data in stub.GetTrainingData(training_data_pb2.TrainingDataRequest()):
-			print(data)
+		while True:
+			for data in stub.GetTrainingData(training_data_pb2.TrainingDataRequest()):
+				print(data)
+				text_format.PrintMessage(data, file)
 
 
 if __name__ == "__main__":
